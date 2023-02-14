44446f36141d01524b539c9f47a8aabfb48d80d3
Georgios Pavlis <geomp03@gmail.com>
2023-02-14 20:05:43 +0000

    Clean-up & Bug fixes
    Clean-up some of the player code logic
    
    Redefined the death parameter and how it is used.
    
    Fixed player P2 death animation.
    
    Simplified animators by removing unused animations

--- F21GP_CW1/Assets/Player.cs
+++ F21GP_CW1/Assets/Player.cs
@@ -18,8 +18,7 @@ public class Player : MonoBehaviour
     public  int   MaxHealth = 10;
     public  int   CurrentHealth_P1;
     public  int   CurrentHealth_P2;
-    public  bool  death_P1;
-    public  bool  death_P2;
+    public  bool  death;
     private float DirX;
     private bool  grounded = false;
     private bool  combatIdle = false;
@@ -34,8 +33,7 @@ public class Player : MonoBehaviour
         CurrentHealth_P1 = MaxHealth;
         CurrentHealth_P2 = MaxHealth;
         healthBar.SetMaxHealth(MaxHealth);
-        death_P1 = false;
-        death_P2 = false;
+        death = false;
 
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
@@ -81,7 +79,7 @@ public class Player : MonoBehaviour
 
 
         // Handle input and movement
-        if(death_P1==false && death_P2==false)
+        if(death==false)
         {
             if (name == "Player_P1")
             {
@@ -110,72 +108,54 @@ public class Player : MonoBehaviour
 
             // Set AirSpeed in animator
             animator.SetFloat("AirSpeed", rb.velocity.y);
+
+
+            // Handle Animations
+
+            // Attack
+            if (name == "Player_P1" && Input.GetButtonDown("Attack_P1"))
+            {
+                // swordHitBox.enabled = true;
+                animator.SetTrigger("Attack");
+            }
+            else if (name == "Player_P2" && Input.GetButtonDown("Attack_P2"))
+            {
+                animator.SetTrigger("Attack3");
+            }
+
+
+            // Jump
+            if (name == "Player_P1" && Input.GetButtonDown("Jump_P1") && grounded)
+            {
+                animator.SetTrigger("Jump");
+                grounded = false;
+                animator.SetBool("Grounded", grounded);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                groundSensor.Disable(0.2f);
+            }
+            else if (name == "Player_P2" && Input.GetButtonDown("Jump_P2") && grounded)
+            {
+                animator.SetTrigger("Jump");
+                grounded = false;
+                animator.SetBool("Grounded", grounded);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                groundSensor.Disable(0.2f);
+            }
+
+            // Run
+            else if (name == "Player_P1" && Mathf.Abs(DirX) > Mathf.Epsilon)
+                animator.SetInteger("AnimState", 2);
+            else if (name == "Player_P2" && Mathf.Abs(DirX) > Mathf.Epsilon)
+                animator.SetInteger("AnimState", 1);
+
+            // Idle
+            else
+                animator.SetInteger("AnimState", 0);
         }
 
-        // Handle Animations
 
-        // Death
-        if (death_P1 || death_P2)
-            animator.SetTrigger("Death");
-
-
-        // Attack
-        else if (name == "Player_P1" && Input.GetButtonDown("Attack_P1"))
-        {
-            // swordHitBox.enabled = true;
-            animator.SetTrigger("Attack");
-        }
-
-        else if (name == "Player_P2" && Input.GetButtonDown("Attack_P2"))
-        {
-            animator.SetTrigger("Attack3");
-        }
-
-
-        // Change between idle and combat idle
-        else if (Input.GetKeyDown("f"))
-            combatIdle = !combatIdle;
-
-
-        // Jump
-        else if (name == "Player_P1" && Input.GetButtonDown("Jump_P1") && grounded)
-        {
-            animator.SetTrigger("Jump");
-            grounded = false;
-            animator.SetBool("Grounded", grounded);
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            groundSensor.Disable(0.2f);
-        }
-
-        else if (name == "Player_P2" && Input.GetButtonDown("Jump_P2") && grounded)
-        {
-            animator.SetTrigger("Jump");
-            grounded = false;
-            animator.SetBool("Grounded", grounded);
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            groundSensor.Disable(0.2f);
-        }
-
-
-        // Run
-        else if (name == "Player_P1" && Mathf.Abs(DirX) > Mathf.Epsilon)
-            animator.SetInteger("AnimState", 2);
-        else if (name == "Player_P2" && Mathf.Abs(DirX) > Mathf.Epsilon)
-            animator.SetInteger("AnimState", 1);
-
-
-        // Combat Idle
-        else if (combatIdle)
-            animator.SetInteger("AnimState", 1);
-
-
-        // Idle
-        else
-            animator.SetInteger("AnimState", 0);
-
-
-        // Revocer Player after death
-        if ((death_P1==true || death_P2==true) && Input.GetKeyDown(KeyCode.R))
+        // Restart scene after death
+        if ((death==true) && Input.GetKeyDown(KeyCode.R))
         {
             // Debug info
             Debug.Log("Restart requested");
@@ -199,24 +179,31 @@ public class Player : MonoBehaviour
 
     void TakeDamage (int Damage)
     {
-        if (name == "Player_P1" && death_P1 == false)
+        if (name == "Player_P1" && death == false)
         {
             CurrentHealth_P1 -= Damage;
             healthBar.SetHealth(CurrentHealth_P1);
 
             // Check for death
             if (CurrentHealth_P1 <= 0)
-                death_P1 = true;
+            {
+                animator.SetTrigger("Death");
+                death = true;
+            }
+                
         }
 
-        if (name == "Player_P2" && death_P2 == false)
+        if (name == "Player_P2" && death == false)
         {
             CurrentHealth_P2 -= Damage;
             healthBar.SetHealth(CurrentHealth_P2);
 
             // Check for death
             if (CurrentHealth_P2 <= 0)
-                death_P2 = true;
+            {
+                animator.SetTrigger("Death");
+                death = true;
+            }
         }
 
 
